f6ab4fe6a4ee20f8dacbb3651d5b3c4ae6c8b1a4
Alan Hamlett <alan.hamlett@gmail.com>
2013-03-13 16:18:14 -0700

    Re-implemented Todo example to be scalable. Use this pattern if you plan to do more than just a Todo app.
    Changes:
    
    * each Model, View, Collection, and template in separate file
    * templates included using RequireJS text.js plugin
    * Backbone.Router replaces AppView as the global app

---
+++ todos-main.js
@@ -0,0 +1,37 @@
+// RequireJS config
+// ---------------
+
+// Here we setup path aliases and dependencies
+require.config({
+    paths: {
+        'libs': 'spec/support',
+        'jquery': 'spec/support/jquery',
+        'underscore': 'spec/support/underscore',
+        'backbone': 'spec/support/backbone',
+    },
+    shim: {
+        'jquery': {
+            exports: '$',
+        },
+        'underscore': {
+            exports: '_',
+        },
+        'backbone': {
+            deps: ['jquery', 'underscore'],
+            exports: 'Backbone',
+        },
+        'backbone.localStorage': {
+            deps: ['backbone'],
+            exports: 'Backbone.localStorage',
+        }
+    }
+});
+
+// Load todos-router.js as TodosRouter
+require([
+    'todos-router'
+], function(TodosRouter) {
+
+    // We kick things off by creating the **App**.
+    window.app = new TodosRouter();
+});
---
+++ todos-router.js
@@ -0,0 +1,38 @@
+// The Application
+// ---------------
+
+// Our overall **Backbone.Router** is the top-level piece of UI.
+define([
+    'jquery',
+    'backbone'
+], function(_, Backbone) {
+    return Backbone.Router.extend({
+        routes: {
+            // Hashtag routes and their handlers
+            'todos': 'todos',
+
+            // default route, called on Router init
+            '': 'todos'
+        },
+        todos: function() {
+            this.setView({
+                name: 'TodoListView'
+            });
+            return this;
+        },
+        initialize: function(options) {
+            // start Backbone Hashtag routing
+            Backbone.history.start();
+            return this;
+        },
+        setView: function(options) {
+            $.proxy(require(['views/'+options.name], function(View) {
+                if (this.view)
+                    this.view.unbind().remove();
+                this.view = new View(options);
+                $('#todoapp').append(this.view.render().$el);
+            }), this);
+            return this;
+        }
+    });
+});
